c8da83f32e3af7dc0e231b4167cb25e2b902422b
namratayevale2602 <namratayevale7@gmail.com>
2025-07-14 15:15:44 +0530

    sdsdS

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import HeroSection from "./HeroSection";
+import AboutUs from "./AboutUs";
+import Services from "./Services";
+import WhyChooseUsPage from "./WhyChoose";
+import GalleryPage from "./GalleryPage";
+import HowItWorks from "./HowItWorks";
+
+function Home() {
+  console.log("render Home Commponent");
+  return (
+    <div>
+      <HeroSection />
+      <AboutUs />
+      <Services />
+      <GalleryPage />
+      <HowItWorks />
+      <WhyChooseUsPage />
+    </div>
+  );
+}
+
+export default Home;
